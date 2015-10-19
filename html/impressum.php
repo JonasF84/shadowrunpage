60cc7d5f750f18db0df87c1d438a425e2dcaef4f
jfische1 <jonas.fischer@immonet.de>
2015-10-19 11:47:20 +0200

    added impressum

---
+++ html/impressum.php
@@ -0,0 +1,20 @@
+<?php include 'includes/header.php' ?>
+ <div class ="container">
+ <div
+class="row well">
+ <div class="col-md-6 col-xs-12">
+ <h4>Impressum</h4>
+ <p>
+Angaben gemäß § 5 TMG
+ <br>Jonas Fischer
+ <br>Wendlohstraße 133
+ <br>22459
+Hamburg
+ </p>
+ <h5>Kontakt:</h5>
+ <a
+href="mailto:mussichnocheinrichten@lasserraptor.de">mussichnocheinrichten@lasserraptor.de</a>
+</div>
+ </div>
+ </div>
+ <?php include 'includes/footer.php' ?>
--- html/includes/footer.php
+++ html/includes/footer.php
@@ -1,5 +1,14 @@
 <footer class="footer">
-   <a href="impressum.php">Impressum</a>
+  <?php if($_SERVER["PHP_SELF"] == "/shadowrunpage/index.php") {
+    $impressumlink = "html/impressum.php";
+  }
+  else {
+    $impressumlink = "impressum.php";
+  }
+  ?>
+  <div class="container footer">
+   <a class="footerlink" href="<?= $impressumlink ?>">Impressum</a>
+ </div>
 </body>
 </html>
 </footer>
